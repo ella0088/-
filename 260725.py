903bba28c9784812e08cd6362e026dd3ab170aaf
ella0088 <ella2007000@gmail.com>
2026-07-28 15:08:23 +0900

    Add files via upload

---
+++ 260725.py
@@ -0,0 +1,26 @@
+##name=input("닉네임을 입력하세요 :")
+##print("환영합니다", name,"님!")
+
+##num1=float(input("실수를 입력하세요 :"))
+##print(f'{num1*10:.3f}')
+##                
+
+##a=int(input("첫번째 숫자를 입력하세요 :"))
+##b=int(input("두번째 숫자를 입력하세요 :"))
+##c=int(input("세번째 숫자를 입력하세요 :"))
+##
+##sum=a+b+c
+##
+##print(sum)
+
+
+##r=int(input("반지름을 입력하세요:"))
+##pi=3.14
+##S=r*r*pi
+##l=r*2*pi
+##
+##print(f'원의 넓이는 {S:.1f}')
+##print(f'원의 둘레길이는 {l:.1f}')
+
+
+s
