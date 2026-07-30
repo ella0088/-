73845aa8240aea2d43e4b425dd707ed9396d80ed
ella0088 <ella2007000@gmail.com>
2026-07-30 16:51:45 +0900

    Add files via upload

---
+++ 260730.py
@@ -0,0 +1,155 @@
+##name=input("영화 제목을 입력하세요:")
+##year=input("개봉 연도를 입력하세요:")
+##acr=input("주연 배우를 입력하세요:")
+##
+##print("{}은(는) {}년에 개봉한 {} 주연의 영화입니다.".format(name, year, acr))
+
+##id=input("아이디를 입력해주세요(6글자 이상):")
+##k=id[:4]+"*"*5
+##print("암호화된 아이디 :"+k)
+
+##num=int(input("숫자를 입력해주세요 :"))
+##print(num+50)
+##print(num-50)
+##print(num*50)
+##print(f'{num/50:.3f}')
+
+##w1=3
+##w2=4
+##h=5
+##print(f'사다리꼴의 넓이는 ({w1} + {w2}) * {h} / {2} = {(w1+w2)*h/2}')
+
+##num=int(input("숫자를 입력해주세요 :"))
+##print(num//4)
+##print(num%4)
+##print(num**4)
+
+##w=float(input('몸무게(kg)를 입력하세요 :'))
+##h=float(input('키(m)를 입력하세요 :'))
+##BMI=w/h**2
+##
+##print(f' BMI 지수는 {BMI:.2f}')
+
+##m=1000
+##p=int(input("물건의 가격을 입력합니다 :"))
+##
+##a=(m-p)//500
+##q=(m-p)%500
+##
+##b=q//100
+##w=q%100
+##
+##c=w//50
+##e=w%50
+##
+##d=e//10
+##
+##print(f'500원 {a}개')
+##print(f'100원 {b}개')
+##print(f'50원 {c}개')
+##print(f'10원 {d}개')
+
+##num=int(input("다섯 자리 숫자를 입력합니다 :"))
+##num1=num%10
+##num = num //10
+##num10=num%10
+##num = num //10
+##num100=num%10
+##num = num //10
+##num1000=num%10
+##num = num //10
+##num10000=num%10
+##
+##print(f'만의 자리 : {num10000}')
+##print(f'천의 자리 : {num1000}')
+##print(f'백의 자리 : {num100}')
+##print(f'십의 자리 : {num10}')
+##print(f'일의 자리 : {num1}')
+
+##num1, num2, num3=input("숫자 3개를 입력해주세요 :").split()
+##
+##num=100
+##num-=int(num1)
+##num-=int(num2)
+##num-=int(num3)
+##
+##print(num)
+##
+##
+##num=int(input("숫자를 입력해주세요 :"))
+##txt="삐약"
+##txt*=num
+##print(txt)
+##
+##num_ori=1357
+##num=num_ori
+##num%=10
+##print(num)
+##num_ori//=10
+##num=num_ori
+##num%=10
+##print(num)
+##num_ori//=10
+##num=num_ori
+##num%=10
+##print(num)
+##num_ori//=10
+##num=num_ori
+##num%=10
+##print(num)
+##
+##num1, num2, num3, num4, num5= input("숫자 5개를 입력해주세요 :").split()
+##avg=(int(num1)+int(num2)+int(num3)+int(num4)+int(num5))/5
+##print(f'{avg:.3f}')
+##
+##message=input("문자열을 입력해주세요 :")
+##txt1, txt2=message.split()
+##
+##txt= txt1[0].upper()
+##txt+=txt1[1:]
+##
+##txt3=txt2[0].upper()
+##txt3+=txt2[1:]
+##
+##print(txt,txt3)
+
+##num=int(input("숫자를 입력해주세요 :"))
+##print(num<100)
+##
+##
+##num=int(input("숫자를 입력해주세요 :"))
+##print(30<num<60)
+##
+##
+##num=int(input("숫자를 입력해주세요 :"))
+##print(num==0 or num==10 or num==100)
+##
+##num=int(input("숫자를 입력해주세요 :"))
+##print(num!=0 and (num%2==1 or num%8==0))
+##
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
