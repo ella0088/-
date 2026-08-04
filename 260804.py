b3c52f6fc05a175e71a9c3428bace39829bf5991
ella0088 <ella2007000@gmail.com>
2026-08-04 16:52:21 +0900

    Add files via upload

---
+++ 260804.py
@@ -0,0 +1,252 @@
+##num=int(input('숫자를 입력해주세요 : '))
+##
+##if num%2==0:
+##    print(f'{num}는 짝수입니다.')
+
+##tem=int(input('현재 온도를 입력해주세요 : '))
+##
+##if tem<25:
+##    print('날씨 상태: 시원한 날씨')
+##if 25<=tem<30:
+##    print('날씨 상태: 따뜻한 날씨')
+##if 30<=tem:
+##    print('날씨 상태: 더운 날씨')    
+
+##message=input("문자열을 입력하세요 :")
+##
+##if ' ' in message:
+##    print("공백이 포함되어 있습니다.")
+##else:
+##    print("공백이 포함되어 있지 않습니다.")
+
+##key=input("방향키를 입력해주세요 (w/a/s/d) : ")
+##if key=='w':
+##    print('위 방향키를 입력하셨습니다.')
+##elif key=='a':
+##    print('좌 방향키를 입력하셨습니다.')
+##elif key=='s':
+##    print('아래 방향키를 입력하셨습니다.')
+##elif key=='d':
+##    print('우 방향키를 입력하셨습니다.')
+##else:
+##    print('잘못 입력하셨습니다.')
+##
+##num=int(input("숫자를 입력해주세요 : "))
+##
+##if num<0:
+##        print("number은 음수입니다.")
+##elif num>0:
+##        print("number은 양수입니다.")
+##else:
+##    print("number은 0입니다.")
+##
+##if num%2==0:
+##    print("number는 짝수입니다.")
+##else:
+##    print("number는 홀수입니다.")
+##
+##
+##num=int(input("숫자를 입력해주세요 : "))
+##
+##if num%3==0:
+##    print("이 숫자는 3의 배수입니다.")
+##    if num%6==0:
+##        print("이 숫자는 6의 배수입니다.")
+##    if num%9==0:
+##        print("이 숫자는 9의 배수입니다.")
+##
+##txt=input("알파벳을 입력해주세요 : ")
+##
+##low='abcdefghijklmnopqrstuvwxyz'
+##up='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+##
+##if txt in low:
+##    print("소문자입니다.")
+##if txt in up:
+##    print("대문자입니다.")
+
+##num=int(input("숫자를 입력해주세요 : "))
+##
+##if 10<=num<=99:
+##    print("두자리 숫자입니다.")
+##else:
+##    print("두자리 숫자가 아닙니다.")
+##
+##num=int(input("숫자를 입력해주세요.(0~100) : "))
+##
+##if 45<=num<=55:
+##    print("Perfect!!")
+##elif 35<=num<=65:
+##    print("Excellent!!")
+##else:
+##    print("Good!!")
+##    
+##year=int(input("연도를 입력해주세요 : "))
+##
+##if (year%4==0 and year%100!=0) or year%400==0:
+##    print("윤년입니다.")
+##else:
+##    print("윤년이 아닙니다.")
+##
+##num1=int(input("첫번째 숫자를 입력해 주세요 : "))
+##num2=int(input("두번째 숫자를 입력해 주세요 : "))
+##num3=int(input("세번째 숫자를 입력해 주세요 : "))
+##
+##if num1<num2<num3:
+##    print(num1,num2,num3)
+##elif num1<num3<num2:
+##    print(num1,num3,num2)
+##elif num3<num2<num1:
+##    print(num3,num2,num1)
+##elif num3<num1<num2:
+##    print(num3,num1,num2)
+##elif num2<num1<num3:
+##    print(num2,num1,num3)
+##else:
+##    print(num2,num3,num1)
+##
+##num01=0
+##num02=0
+##num03=0
+##
+##num1=int(input("첫번째 숫자를 입력해 주세요 : "))
+##num2=int(input("두번째 숫자를 입력해 주세요 : "))
+##
+##if num1<num2:
+##    num01=num1
+##    num02=num2
+##elif num2<num1:
+##    num01=num2
+##    num02=num1
+##
+##num3=int(input("세번째 숫자를 입력해 주세요 : "))
+##
+##if num3>num02:
+##    num03=num3
+##elif num01<num3<num02:
+##    num03=num02
+##    num02=num3
+##elif num3<num01<num02:
+##    num03=num02
+##    num02=num01
+##    num01=num3
+##    
+##print(num01,num02,num03)
+##
+##num01=0
+##num02=0
+##num03=0
+##
+##num1=int(input("첫번째 숫자를 입력해 주세요 : "))
+##num2=int(input("두번째 숫자를 입력해 주세요 : "))
+##
+##if num1<num2:
+##    num01=num1
+##    num02=num2
+##elif num2<num1:
+##    num01=num2
+##    num02=num1
+##
+##num3=int(input("세번째 숫자를 입력해 주세요 : "))
+##
+##if num3>num02:
+##    num03=num3
+##elif num01<num3<num02:
+##    num03=num02
+##    num02=num3
+##elif num3<num01<num02:
+##    num03=num02
+##    num02=num01
+##    num01=num3
+##    
+##if num03-num01==2:
+##    print("스트레이트!")
+##else:
+##    print("X")
+
+
+##count=0
+##
+##while count<5:
+##    print("파이썬")
+##    count+=1
+
+##count=1
+##num=0
+##
+##while count<=10:
+##    num+=count
+##    count+=1
+##
+##print(num)
+
+##num=5
+##while num<=200:
+##    print(f'{num}',end=" ")
+##    num+=5
+    
+##num=5
+##while num<=200:
+##    print(f'{num}',end=" ")
+##    if num%25==0:
+##        print("\n")
+##    num+=5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
